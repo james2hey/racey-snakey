bdaeabc9c36d246cb6d952046e0f56b0a25172fe
Gerald Craig Toft <gto23@cs14153jm.canterbury.ac.nz>
2017-10-15 11:03:08 +1300

    two snakes take two

--- communications.c
+++ communications.c
@@ -6,25 +6,31 @@ void receive_snake(snake_t* snake_buffer) {
     timer_tick_t end_time = now + READ_TIMEOUT * TIMER_RATE;
     char read;
     uint8_t num_read = 0;
+    uint8_t snake_length = MAX_SNAKE_LENGTH + 1;
     
-    while (end_time - now < TIMER_OVERRUN_MAX && num_read < MAX_SNAKE_LENGTH) {
+    while (end_time - now < TIMER_OVERRUN_MAX && num_read < snake_length) {
         if (ir_uart_read_ready_p()) {
             read = ir_uart_getc();
-            snake_buffer->tail[num_read].x = read % TINYGL_WIDTH;
-            snake_buffer->tail[num_read].y = read / TINYGL_WIDTH;
-            num_read++;
+            if (snake_length > MAX_SNAKE_LENGTH) {
+                if (read >= 0 && read <= MAX_SNAKE_LENGTH) {
+                    snake_length = read;
+                    snake_buffer->cur_length = snake_length;
+                }
+            } else {
+                snake_buffer->tail[num_read].x = read % TINYGL_WIDTH;
+                snake_buffer->tail[num_read].y = read / TINYGL_WIDTH;
+                num_read++;
+            }
         }
     }
-    
-    snake_buffer->cur_length = num_read;
 }
 
 
 void send_snake(snake_t* snake) {
-    char snake_string[snake->cur_length + 1];
+    char snake_string[snake->cur_length + 2];
     uint8_t i = 0;
     for (i = 0; i < snake->cur_length; i++) {
-        snake_string[i] = TINYGL_WIDTH * snake->tail[i].y + snake->tail[i].x;
+        snake_string[i + 1] = TINYGL_WIDTH * snake->tail[i].y + snake->tail[i].x;
     }
     snake_string[snake->cur_length] = '\0';
     ir_uart_puts(snake_string);
